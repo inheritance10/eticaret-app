648bdd8f16b463a09f2c581644601cd11e3c98fe
inheritance10 <ali552001a@gmail.com>
2022-10-18 03:00:16 +0300

    sepet,kategori,urun,anasayfa,siparisler ve detaylari,odeme sayfalari,route'lari ve controller'lar olusturuldu.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainPageController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\OrderController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('mainpage');
-});
+Route::get('/',[MainPageController::class,'index'])->name('mainpage');
+Route::get('category/{slug_categoryName}',[CategoryController::class,'index'])->name('category');
+Route::get('product/{slug_productName}',[ProductController::class,'index'])->name('product');
+Route::get('cart',[CartController::class,'index'])->name('cart');
+Route::get('payment',[PaymentController::class,'index'])->name('payment');
+Route::get('order',[OrderController::class,'index'])->name('order');
+Route::get('order-detail/{id}',[OrderController::class,'orderDetail'])->name('orderDetail');
