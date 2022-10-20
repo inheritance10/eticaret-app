435cb34e6579ae9775041598289126e957466535
inheritance10 <ali552001a@gmail.com>
2022-10-20 03:32:38 +0300

    register ve login sayfalari olusturuldu.Kategori tablosu kuruldu.

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\OrderController;
-
+use App\Http\Controllers\UserController;
 
 Route::get('/',[MainPageController::class,'index'])->name('mainpage');
 Route::get('category/{slug_categoryName}',[CategoryController::class,'index'])->name('category');
@@ -16,3 +16,9 @@ Route::get('cart',[CartController::class,'index'])->name('cart');
 Route::get('payment',[PaymentController::class,'index'])->name('payment');
 Route::get('order',[OrderController::class,'index'])->name('order');
 Route::get('order-detail/{id}',[OrderController::class,'orderDetail'])->name('orderDetail');
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/login',[UserController::class,'login'])->name('login');
+    Route::get('/register', [UserController::class,'register'])->name('register');
+});
+
